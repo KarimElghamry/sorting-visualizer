118dc41fa5125c0248e870b7697ed65db79ec40e
KarimElghamry <Karimelghamry@gmail.com>
2021-01-08 16:18:23 +0200

    add: visualization for selection sort

--- src/components/sorting_bar/SortingBar.jsx
+++ src/components/sorting_bar/SortingBar.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
 const SortingBar = (props) =>{
+    let color = '#efefef';
+
+    switch (props.colorCode) {
+        case 1:
+            color = 'green';
+            break;
+        case 2:
+            color = 'red';
+            break;
+        default:
+            break;
+    }
 
     return(
-    <div style={{...props.style, backgroundColor:'#efefef'}}>
+    <div style={{...props.style, backgroundColor: color}}>
     
     </div>
     );
--- src/helpers/randomizeArray.js
+++ src/helpers/randomizeArray.js
@@ -2,7 +2,7 @@ const generateRandomizedArray = ({arraySize} = {}) =>{
     let randomizedArray = [];
 
     for(let i = 0; i < arraySize;i++){
-        randomizedArray.push(Math.floor(Math.random() * 300));
+        randomizedArray.push(Math.floor(Math.random() * (300 - 5) + 5));
     }
     return randomizedArray;
 }
